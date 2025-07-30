b99e65c2a55766b3476b7493c465d34a761c0d52
raileats <raileats@gmail.com>
2025-07-31 03:19:43 +0530

    Update App.js

--- src/src/App.js
+++ src/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import HeroBanner from "./components/HeroBanner";
+import CategoryCards from "./components/CategoryCards";
+import PopularItems from "./components/PopularItems";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to RailEats</h1>
+      <HeroBanner />
+      <CategoryCards />
+      <PopularItems />
     </div>
   );
 }
 
 export default App;
-Add src/App.js main component
