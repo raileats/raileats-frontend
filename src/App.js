138d7247696ef1a3d5c287ca44818e919dda0b4d
raileats <raileats@gmail.com>
2025-07-31 15:23:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 // src/App.js
 import React from "react";
-import HeroBanner from "./components/HeroBanner";
-import CategoryCards from "./components/CategoryCards";
-import PopularItems from "./components/PopularItems";
-import SearchByPNR from "./components/SearchByPNR";
-import SearchByTrain from "./components/SearchByTrain";
+import AdminPanel from "./AdminPanel"; // Import admin layout
 
 function App() {
   return (
     <div>
-      <HeroBanner />
-      <SearchByPNR />
-      <SearchByTrain />
-      <CategoryCards />
-      <PopularItems />
+      <AdminPanel />
     </div>
   );
 }
