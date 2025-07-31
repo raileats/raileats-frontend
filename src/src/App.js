096a66aabb32566a03587b1772d7a0473bed3453
raileats <raileats@gmail.com>
2025-07-31 13:50:23 +0530

    Update App.js

--- src/src/App.js
+++ src/src/App.js
@@ -1,20 +1,12 @@
-// ✅ App.js
-
 import React from 'react';
-import HeroBanner from './components/HeroBanner';
 import CategoryCards from './components/CategoryCards';
-import PopularItems from './components/PopularItems';
-import SearchByPNR from './components/SearchByPNR';
-import SearchByTrain from './components/SearchByTrain';
 
 function App() {
   return (
-    <div className="app">
-      <HeroBanner />
-      <SearchByPNR />
-      <SearchByTrain />
+    <div className="App">
+      {/* Existing content */}
       <CategoryCards />
-      <PopularItems />
+      {/* Other components */}
     </div>
   );
 }
