15fef8fca0a0cabd3a8832a10690c8a90946afed
raileats <raileats@gmail.com>
2025-07-31 15:32:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 // src/App.js
-import React from "react";
-import AdminPanel from "./AdminPanel"; // Import admin layout
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminSidebar from './components/AdminSidebar';
+import Dashboard from './components/Dashboard';
+import Orders from './components/Orders';
+import Outlets from './components/Outlets';
+import Menu from './components/Menu';
+import Master from './components/Master';
+import Train from './components/Train';
+import Logout from './components/Logout';
 
 function App() {
   return (
-    <div>
-      <AdminPanel />
-    </div>
+    <Router>
+      <div className="admin-layout">
+        <AdminSidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/outlets" element={<Outlets />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/master" element={<Master />} />
+            <Route path="/train" element={<Train />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
