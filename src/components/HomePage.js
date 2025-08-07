57003f37dd36860f64cb69854e004ebadabcd706
raileats <raileats@gmail.com>
2025-08-08 05:13:17 +0530

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,20 @@
+// src/components/HomePage.js
 import React from "react";
 import HeroBanner from "./HeroBanner";
 import OffersSlider from "./OffersSlider";
 import CategoryCards from "./CategoryCards";
 import PopularItems from "./PopularItems";
+import Footer from "./Footer";
 
 export default function HomePage() {
   return (
     <div>
       <HeroBanner />
       <OffersSlider />
+      <div id="order"></div> {/* anchor for Order Now */}
       <CategoryCards />
       <PopularItems />
+      <Footer />
     </div>
   );
 }
